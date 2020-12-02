5e26e6a27257fc54fa6651bc32a9e9d695b72909
jsj2573 <63095031+jsj2573@users.noreply.github.com>
2020-12-02 17:27:57 +0900

    add question

--- narcissism/app.js
+++ narcissism/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/category');
 var searchingRouter = require('./routes/searching');
+var questionRouter = require('./routes/question');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/category', categoryRouter);
+app.use('/searching', searchingRouter);
+app.use('/question', questionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

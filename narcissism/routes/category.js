85e7d3c8ae4048731db29536c1c3dc080befefeb
jsj2573 <63095031+jsj2573@users.noreply.github.com>
2020-12-02 18:05:03 +0900

    add categoryId

--- narcissism/routes/category.js
+++ narcissism/routes/category.js
@@ -1,8 +1,17 @@
 var express = require('express');
+const { request, response } = require('../../../../../Desktop/서버 실험/myapp/app');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/:categoryId',(request, response) => {
+    id = request.params.categoryId;
+    // db 에서 데이터 조회한 뒤 json으로 반환
+    response.json({categoryId:id})
+})
+
 router.get('/', function(req, res, next) {
+  // db 에서 데이터 조회한 뒤 json으로 반환
   res.json({ title: 'Express' });
 });
 

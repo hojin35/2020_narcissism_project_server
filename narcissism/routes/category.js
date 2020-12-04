c07947ca8bcbf9e921ae70116a936ca6ab3ff997
jsj2573 <63095031+jsj2573@users.noreply.github.com>
2020-12-04 17:16:10 +0900

    add categoryId, questionId

--- narcissism/routes/category.js
+++ narcissism/routes/category.js
@@ -7,11 +7,24 @@ var conn = db_config.init();
 
 router.get('/:categoryId',(request, response) => {
     id = request.params.categoryId;
+    console.log(id);
+    sql = 'SELECT * FROM question WHERE categoryId = '+id;
+    conn = db_config.init();
+    conn.query( sql, function(err, results, fields) {
+      if (err) {
+        console.log(err);
+        conn.end();
+        response.json({error:err});
+      }
+      console.log(results);
+      conn.end();
+      response.json(results);
+    });
     // db 에서 데이터 조회한 뒤 json으로 반환
-    response.json({categoryId:id})
 })
 
 router.get('/', function(req, res, next) {
+  conn = db_config.init();
   conn.query('SELECT * FROM category', function(err, results, fields) {
     if (err) {
       console.log(err);
--- narcissism/routes/question.js
+++ narcissism/routes/question.js
@@ -1,15 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var db_config = require('./dbconfig');
+var conn = db_config.init();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 router.get('/:questionId',(request, response) => {
-  id = request.params.questionId;
-  // db 에서 데이터 조회한 뒤 json으로 반환
-  response.json({questionId:id})
+    id = request.params.questionId;
+    console.log(id);
+    sql = 'SELECT * FROM answer WHERE questionId = '+id;
+    conn = db_config.init();
+    conn.query( sql, function(err, results, fields) {
+      if (err) {
+        console.log(err);
+        conn.end();
+        response.json({error:err});
+      }
+      console.log(results);
+      conn.end();
+      response.json(results);
+    });
+    // db 에서 데이터 조회한 뒤 json으로 반환
 })
+
+router.get('/', function(req, res, next) {
+  conn = db_config.init();
+  conn.query('SELECT * FROM question', function(err, results, fields) {
+    if (err) {
+      console.log(err);
+      conn.end();
+      res.json({error:err});
+    }
+    console.log(results);
+    conn.end();
+    res.json(results);
+  });
+  
+
+  //res.json({title: 'Express' });
+});
 
 module.exports = router;
